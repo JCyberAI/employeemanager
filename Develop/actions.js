f5a85c6b4bbe9e0f35538a422c3627eca4f5535d
Jalyn Campbell <jalyn.campbell@yahoo.com>
2021-03-10 16:10:19 -0500

    added & edited action.js

---
+++ Develop/actions.js
@@ -0,0 +1,20 @@
+const actions = [
+    {
+      type: 'list',
+      loop: false,
+      name: 'choices',
+      message: `What would you like to do?`,
+      choices: ['View Departments', 'View Roles', 'View Employees', 'Add Department', 'Add Roles', 'Add Employee', 'Update Employee Roles', 'Quit']
+    },
+    {
+      type: 'confirm',
+      name: 'quit',
+      message: 'Are you sure you want to quit the application?',
+      default: false,
+      when: (answers) => {
+        return answers.choices === 'Quit';
+      }
+    }
+  ];
+  
+  module.exports = actions;

a316f30a1d97cb67eac5ed402c053813558498c4
Jalyn Campbell <jalyn.campbell@yahoo.com>
2021-03-10 16:25:56 -0500

    edited indexjs file

--- Develop/index.js
+++ Develop/index.js
@@ -1,2 +1,13 @@
 require ("dotenv").config();
 const actions = require("./actions");
+const inquirer = require("inquirer");
+const mysql = require("sql");
+const dbmain = process.env.DB_HOST;
+const dbsub = process.env.DB_USER;
+const dbpass = process.env.DB_PASS;
+const db = mysql.createConnection({
+    host,
+    user,
+    password,
+    database
+})

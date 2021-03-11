701ba2cd9799f961c60e893ff3fcac781cdd8e80
Jalyn Campbell <jalyn.campbell@yahoo.com>
2021-03-11 14:09:32 -0500

    edited index js file

--- Develop/index.js
+++ Develop/index.js
@@ -11,3 +11,57 @@ const db = mysql.createConnection({
     dbpass,
     db
 })
+
+const prompt = () => {
+    inquirer
+      .prompt(actions)
+      .then(answers => {
+        switch (answers.choices) {
+          case 'View Employees':
+            viewEmployees();
+            break;
+  
+          case 'View Departments':
+            viewDepartments();
+            break;
+  
+          case 'View Roles':
+            viewRoles();
+            break;
+  
+          case 'Add Employee':
+            addEmployee();
+            break;
+  
+          case 'Add Department':
+            addDepartment();
+            break;
+  
+          case 'Add Roles':
+            addRole();
+            break;
+  
+          case 'Quit':
+            delete answers.choices;
+            quit(answers);
+            break;
+        }
+      })
+      .catch(err => {
+        if (err) throw err;
+      })
+  }
+// const initQuestions = require('./lib/questions');
+// const inquirer = require('inquirer');
+// const table = require('console.table');
+// const mysql = require('mysql');
+// const host = process.env.DB_HOST;
+// const user = process.env.DB_USER;
+// const password = process.env.DB_PASS;
+// const database = process.env.DB_NAME;
+// const db = mysql.createConnection({
+//   host,
+//   user,
+//   password,
+//   database
+// })

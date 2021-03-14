aae1870a55badb3e33e13d86d6cf26d84d56886a
Jalyn Campbell <jalyn.campbell@yahoo.com>
2021-03-14 15:53:00 -0400

    edited importer js file

--- Develop/importer.js
+++ Develop/importer.js
@@ -3,11 +3,18 @@ const Importer = require('mysql-import');
 const host = process.env.DB_HOST;
 const user = process.env.DB_USER;
 const password = process.env.DB_PASS;
-const importer = new Importer({host, user, password});
+const importer = new Importer({host, user, password, database});
 
 importer.onProgress(progress=>{
   let percent = Math.floor(progress.bytes_processed / progress.total_bytes * 10000) / 100;
   console.log(`${percent}% Completed`);
+});
+
+importer.import('./sample-data/sample-data.sql').then(() => {
+  let files_imported = importer.getImported(); 
+  console.log(`${files_imported.length} SQL file(s) imported.`);
+}).catch(err => {
+  console.log(err);
 });
 
 importer.import('emsdb.sql').then(() => {
